12d4ee609df00d21fd3269113c9f24a812845419
Marlon Lira <marlonlira3@gmail.com>
2020-01-07 15:26:05 -0300

    ajustes para o heruko

--- commons/configs/Server.ts
+++ commons/configs/Server.ts
@@ -1,16 +1,16 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser'
+import * as bodyParser from 'body-parser';
 
-const server = express()
-const allowCors = require('./Cors')
-const port = 4001
+const server = express();
+const allowCors = require('./Cors');
+const port = process.env.port || 4001;
 
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
-server.use(allowCors)
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
-})
+});
 
-module.exports = server
+module.exports = server;
