4440a8fc67d0bfba109620b6a6add7beec2ef887
Marlon Lira <38389561+MarlonLira@users.noreply.github.com>
2020-04-10 20:43:03 -0300

    Update context.ts

--- main/context.ts
+++ main/context.ts
@@ -3,7 +3,7 @@ import * as Config from '../config.json';
 import Attributes from "../commons/core/attributes";;
 
 var _logging = Config.Database.Logging;
-var _dbConfig = Config.Database.MSSQL;
+var _dbConfig = Config.Database.PostgreSQL;
 
 /**
  * @description
@@ -24,14 +24,14 @@ class Context {
       {
         port: _dbConfig.port,
         host: _dbConfig.host,
-        dialect: 'mssql',
-        logging: _logging,
-        dialectOptions: {
-          options: {
-            trustServerCertificate: true
-          },
-          ssl: true
-        }
+        dialect: 'postgres',
+        logging: _logging
+        // dialectOptions: {
+        //   options: {
+        //     trustServerCertificate: true
+        //   },
+        //   ssl: true
+        // }
       }
     );
     return sequelize;
