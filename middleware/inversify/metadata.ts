d914d5443211de0f93768e4ef1d3c85194a13077
Marlon Jose de Oliveira Lira <m.de.oliveira.lira@avanade.com>
2020-05-04 14:25:25 -0300

    fix(production)

--- middleware/inversify/metadata.ts
+++ middleware/inversify/metadata.ts
@@ -2,9 +2,9 @@
 import '../../data/controllers/userController';
 import '../../data/controllers/userAdressController';
 import '../../data/controllers/authController';
-import '../../data/controllers/CardController';
-import '../../data/controllers/CompanyController';
-import '../../data/controllers/CompanyAdressController';
+import '../../data/controllers/cardController';
+import '../../data/controllers/companyController';
+import '../../data/controllers/companyAdressController';
 import '../../data/controllers/employeeController';
 import '../../data/controllers/paymentController';
 import '../../data/controllers/parkingController';
