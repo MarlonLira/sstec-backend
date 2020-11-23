e9ff7226c6b31ce8fd5137e2c6be5274e0443779
Marlon Lira <38389561+MarlonLira@users.noreply.github.com>
2020-11-22 23:48:50 -0300

    Fix auth user (#257)
    * info(database)
    
    * info
    
    * fix(parking-price): ajustes no model
    
    * fix(parking): ajustes no metodo de update
    
    * fix(auth): correção no signup do user

--- data/interfaces/IServices/authService.interface.ts
+++ data/interfaces/IServices/authService.interface.ts
@@ -3,8 +3,6 @@ import { Auth } from '../../models/auth.model';
 export interface IAuthService {
   checkToken(token: string): Promise<any>;
   accountRecoveryEmployee(auth: Auth): Promise<any>;
-  createEmployeeToken(auth: Auth): Promise<Auth>;
-  createUserToken(auth: Auth): Promise<Auth>;
   signinEmployee(auth: Auth): Promise<any>;
   signinUser(auth: Auth): Promise<any>;
   signupCompany(auth: Auth): Promise<any>;
